28348b5e2db1958c26133244eb5fd52321148f96
Eulalio Medina U20 <admin@lemonwebdesign.mx>
2021-02-27 17:21:37 -0600

    carga de primeros archivos al epositorio

---
+++ database/migrations/2021_02_27_213017_create_app_settings_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAppSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('app_settings');
+
+        Schema::create('app_settings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('layout')->default(1);
+            $table->integer('sidebar')->default(1);
+            $table->integer('navbar')->default(1);
+            $table->timestamps();
+            $table->softDeletes();
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->integer('created_by')->unsigned()->nullable();
+            $table->integer('updated_by')->unsigned()->nullable();
+
+            //Indexs
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->foreign('updated_by')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('app_settings');
+    }
+}
