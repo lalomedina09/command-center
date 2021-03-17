c5190f7156d845f1eae79e084a69141f8ca10e41
Eulalio Medina U20 <admin@lemonwebdesign.mx>
2021-03-16 21:28:31 -0600

    modelos de DB

--- app/Models/CategoryProduct.php
+++ app/Models/CategoryProduct.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Contact extends Model
+class CategoryProduct extends Model
 {
     use HasFactory;
     use SoftDeletes;
--- app/Models/SocialProfile.php
+++ app/Models/SocialProfile.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Contact extends Model
+class SocialProfile extends Model
 {
     use HasFactory;
     use SoftDeletes;
